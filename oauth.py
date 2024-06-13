28780181f0f9bd5b8bb642a2a0712d900acfb43c
AyenamKhanal <ayenam.khanal@students.mq.edu.au>
2024-06-13 23:56:08 +1000

    separated oauth and main file

---
+++ oauth.py
@@ -0,0 +1,89 @@
+from dotenv import load_dotenv
+import os
+from urllib.parse import urlencode
+from requests import post, get
+from flask import Flask, render_template, redirect, request, jsonify, session
+from datetime import datetime
+
+from helpers import access_required, session_expiry
+
+load_dotenv()
+app = Flask(__name__)
+app.secret_key = "phenom"
+
+CLIENT_ID = os.getenv("CLIENT_ID")
+CLIENT_SECRET = os.getenv("CLIENT_SECRET")
+REDIRECT_URI = "http://localhost:5000/callback"
+AUTH_URL = "https://accounts.spotify.com/authorize"
+TOKEN_URL = "https://accounts.spotify.com/api/token"
+API_BASE_URL = "https://api.spotify.com/v1/"
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/login')
+def login():
+    scope = 'user-top-read user-library-read'
+
+    params = {
+        'client_id' : CLIENT_ID,
+        'response_type' : 'code',
+        'redirect_uri' : REDIRECT_URI,
+        'scope' : scope, 
+        'show dialog': True
+    }
+
+    auth_url = f"{AUTH_URL}?{urlencode(params)}"
+
+    return redirect(auth_url)
+
+@app.route('/callback')
+def callback():
+    if 'error' in request.args:
+        return jsonify({"error" : request.args['error']})
+    
+    if 'code' in request.args:
+        req_body = {
+            'code': request.args['code'],
+            'grant_type': 'authorization_code',
+            'redirect_uri': REDIRECT_URI,
+            'client_id': CLIENT_ID,
+            'client_secret': CLIENT_SECRET
+        }
+
+        response = post(TOKEN_URL, data=req_body)
+        token_info = response.json()
+
+        session['access_token'] = token_info['access_token']
+        session['refresh_token'] = token_info['refresh_token']
+        session['expires_at'] = datetime.now().timestamp() + token_info['expires_in']
+
+    return render_template('page1.html')
+
+
+def generate_headers():
+    return  {'Authorization': f"Bearer {session['access_token']}"}
+
+
+@app.route("/refresh-token")
+def refresh_token():
+    if 'refresh_token' not in session:
+        return redirect('/login')
+    
+    if datetime.now().timestamp() > session['expires_at']:
+        req_body = {
+            'grant_type': 'refresh_token',
+            'refresh_token': session['refresh_token'],
+            'client_id': CLIENT_ID,
+            'client_secret': CLIENT_SECRET
+        }
+        response = post(TOKEN_URL, data=req_body)
+        
+        new_token_info = response.json()
+
+        session['access_token'] = new_token_info['access_token']
+        session['expires_at'] = datetime.now().timestamp() + new_token_info['expires_in'] 
+
+        return redirect('/playlists')
+    
