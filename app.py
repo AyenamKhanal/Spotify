36d5ccef0f6b30d13e31ba19784dafd2a1848746
AyenamKhanal <161411334+AyenamKhanal@users.noreply.github.com>
2024-06-20 17:17:23 +1000

    latest until log in page

--- app.py
+++ app.py
@@ -81,5 +81,11 @@ def refresh_token():
 
         return redirect('/playlists')
     
+@app.route("/home")
+def home():
+    return render_template("home.html")
+
+
+    
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
