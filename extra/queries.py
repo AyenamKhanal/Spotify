e9df165aa0ae8f5e217ea872f47fd62e4c90ca82
AyenamKhanal <161411334+AyenamKhanal@users.noreply.github.com>
2024-06-20 23:07:39 +1000

    rendered top tracks and top artists

--- extra/queries.py
+++ extra/queries.py
@@ -15,17 +15,13 @@ def get_top_tracks():
     headers = generate_headers()
 
     # request spotify for user's top tracks
-    response = get(API_BASE_URL + 'me/top/tracks', headers=headers)
+    response = get(API_BASE_URL + 'me/top/tracks?time_range=short_term', headers=headers)
     
     tracks = response.json()
 
     # parse track names from json 
     for track in tracks["items"]:
         track_name.append(track["name"])
-
-    # parse artist names from json 
-    for track in tracks["items"]:
-        track_name.append(track["artists"])
 
     return track_name
 
@@ -40,7 +36,7 @@ def get_top_artists():
     headers = generate_headers()
 
     # request spotify for user's top tracks
-    response = get(API_BASE_URL + 'me/top/artists', headers=headers)
+    response = get(API_BASE_URL + 'me/top/artists?time_range=short_term', headers=headers)
     
     artists = response.json()
 
