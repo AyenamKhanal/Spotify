fc36c3db672fb62a7f60b8cd2050c9ce2125812e
AyenamKhanal <161411334+AyenamKhanal@users.noreply.github.com>
2024-06-24 14:33:19 +1000

    stored username and profile pic in session

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from requests import post, get
 from flask import Flask, render_template, redirect, request, jsonify, session
 from datetime import datetime
 
-from extra.helpers2 import get_home_page_data
+from extra.helpers2 import get_home_page_data, get_user_profile
 
 app = Flask(__name__, template_folder='templates')
 app.secret_key = "phenom"
@@ -96,21 +96,35 @@ def home():
         artist_time_range = request.args.get("artist-time")
     else:
         artist_time_range = "short_term"
+
     #return render_template("test.html", response=[track_time_range, artist_time_range])
 
+    # to get profile pic and username
+    user_profile_data = get_user_profile()
 
+    # get top songs and top artists data for homepage
     homepage_data = get_home_page_data(track_time_range, artist_time_range)
+
+    # store username and profile pic in sessions for future use in navigation bar
+    session["user_profile_pic"] = user_profile_data["user_profile_pic"]
+    session["user_name"] = user_profile_data["user_name"]
 
     return render_template("home.html",
                             tracks=homepage_data["tracks"],
                             artists=homepage_data["artists"],
                             covers=homepage_data["covers"], 
                             artists_profile_pics=homepage_data["artists_profile_pics"], 
-                            user_name=homepage_data["user_name"], 
-                            user_profile_pic=homepage_data["user_profile_pic"],
+                            user_name=session["user_name"], 
+                            user_profile_pic=session["user_profile_pic"],
                             track_time_range=track_time_range,
                             artist_time_range=artist_time_range
                             )
+
+
+@app.route("/song-stats", methods=["GET", "POST"])
+def song_stats():
+
+    return render_template("test.html", user_profile_pic=session["user_profile_pic"], response=session["user_name"])
 
 
 if __name__ == '__main__':
--- extra/helpers2.py
+++ extra/helpers2.py
@@ -8,7 +8,6 @@ def get_home_page_data(track_time_range, artist_time_range):
     # get necessary instances
     top_tracks_instance = topTracks(track_time_range)
     top_artists_instance = topArtists(artist_time_range)
-    user_profile_instance = userProfile()
 
     # get artist name and pic
     homepage_data["artists"] = top_artists_instance.get_top_artists()
@@ -18,8 +17,19 @@ def get_home_page_data(track_time_range, artist_time_range):
     homepage_data["tracks"] = top_tracks_instance.get_top_tracks()
     homepage_data["covers"] = top_tracks_instance.get_album_cover()
 
-    # get user's name and pic
-    homepage_data["user_name"] = user_profile_instance.get_user_name()
-    homepage_data["user_profile_pic"] = user_profile_instance.get_user_profile_pic()
-
     return homepage_data
+
+
+def get_user_profile():
+    
+    # declare dictionary to store user profile data
+    user_profile_data = {}
+    
+    # create a instance
+    user_profile_instance = userProfile()
+
+    # get user's name and pic
+    user_profile_data["user_name"] = user_profile_instance.get_user_name()
+    user_profile_data["user_profile_pic"] = user_profile_instance.get_user_profile_pic()
+
+    return user_profile_data
